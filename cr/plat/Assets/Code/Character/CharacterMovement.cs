900881ec5db428234160c9ee4ade5dfc6f703024
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-12-17 11:08:49 +0100

    movement code

--- cr/plat/Assets/Code/Character/CharacterMovement.cs
+++ cr/plat/Assets/Code/Character/CharacterMovement.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float speed;     
+    public float jumpF;     
+    [SerializeField] private Vector3 movement;
+    [SerializeField] private float xDir;
+    [SerializeField] private bool canJump = true;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        xDir = Input.GetAxis("Horizontal")*speed;
+
+    }
+    void FixedUpdate() 
+    {
+        rb.velocity = new Vector2(xDir*Time.deltaTime, rb.velocity.y); 
+        if(Input.GetButtonDown("Jump") && canJump)
         {
-            Debug.Log("Left");
+            rb.AddForce(new Vector2(0f, jumpF));
+            Debug.Log("Jump");
         }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        canJump = true;
+    }
+    void OnCollisionExit2D(Collision2D other) 
+    {
+        canJump = false;
     }
 }
