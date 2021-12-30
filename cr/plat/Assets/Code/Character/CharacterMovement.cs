75c8e89ed61913b52aa90b77d647e417ba106342
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-12-30 19:24:52 +0100

    new levels double jump

--- cr/plat/Assets/Code/Character/CharacterMovement.cs
+++ cr/plat/Assets/Code/Character/CharacterMovement.cs
@@ -37,14 +37,14 @@ public class CharacterMovement : MonoBehaviour
         spee = speed/reduce;
         jumpF2 = jumpF*1.5f;
         jumpFstart = jumpF;
-        if(GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().CheckCheckPoints() != 0)
-        {
-            transform.position = GameObject.FindGameObjectsWithTag("CheckPoint")[GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().CheckCheckPoints()].transform.position;
-        }
-        else
-        {
-            transform.position = startPosition.position;
-        }    
+        //if(GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().CheckCheckPoints() != 0)
+        //{
+        //    transform.position = GameObject.FindGameObjectsWithTag("CheckPoint")[GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().CheckCheckPoints()].transform.position;
+        //}
+        //else
+        //{
+        //    transform.position = startPosition.position;
+        //}    
     }
 
     // Update is called once per frame
